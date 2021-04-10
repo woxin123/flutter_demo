9911d698b83af40a67b2f5f83021f2715b84e4d7
1293141942@qq.com <1293141942@qq.com>
2021-04-11 00:12:36 +0800

    scroller

--- flutter_personal/lib/home/body.dart
+++ flutter_personal/lib/home/body.dart
@@ -4,8 +4,9 @@ import 'package:flutter/material.dart';
 class PersonalAppBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    String str = "asnfckljasnbcasklcnbsakljcnsalkcnscasc";
     return Padding(
-      padding: EdgeInsets.only(top: 50.0),
+      padding: EdgeInsets.only(top: 20.0),
       child: Container(
         width: MediaQuery.of(context).size.width,
         // child: Stack(
@@ -15,37 +16,36 @@ class PersonalAppBody extends StatelessWidget {
         //         child: _center(context),
         //     )
         //   ],
-        child:
-          Center(
-            child: new Container(
-              width: MediaQuery.of(context).size.width * 0.8,
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisSize: MainAxisSize.min,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Expanded(
-                    child: _left(),
-                    flex: 1,
-                  ),
-                  SizedBox(
-                    width: 30.0,
-                  ),
-                  Expanded(
-                    child: Center(child: _center(context)),
-                    flex: 2,
-                  ),
-                  SizedBox(
-                    width: 30.0,
-                  ),
-                  Expanded(
-                    child: _right(),
-                    flex: 1,
-                  )
-                ],
-              ),
+        child: Center(
+          child: new Container(
+            width: MediaQuery.of(context).size.width * 0.8,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  child: _left(),
+                  flex: 1,
+                ),
+                SizedBox(
+                  width: 30.0,
+                ),
+                Expanded(
+                  child: Center(child: _center(context)),
+                  flex: 2,
+                ),
+                SizedBox(
+                  width: 30.0,
+                ),
+                Expanded(
+                  child: _right(),
+                  flex: 1,
+                )
+              ],
             ),
           ),
+        ),
       ),
     );
   }
@@ -70,10 +70,16 @@ class PersonalAppBody extends StatelessWidget {
   }
 
   Widget _center(BuildContext context) {
-    return Container(
-      height: 600.0,
-      decoration: BoxDecoration(
-        color: Colors.red,
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Container(
+            height: 1009.0,
+            decoration: BoxDecoration(
+              color: Colors.red,
+            ),
+          ),
+        ],
       ),
     );
   }
--- flutter_personal/lib/home/home.dart
+++ flutter_personal/lib/home/home.dart
@@ -1,23 +1,22 @@
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_personal/home/appbar.dart';
 import 'package:flutter_personal/home/body.dart';
 
 class PersonalHomePage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: null,
-      body: new Column(
-        children: [
-          new PersonalAppBar(),
-          new PersonalAppBody(),
-        ],
-      )
-    );
-
+        appBar: null,
+        body: Scrollbar(
+          child: SingleChildScrollView(
+            child: new Column(
+              children: [
+                new PersonalAppBar(),
+                new PersonalAppBody(),
+              ],
+            ),
+          ),
+        ));
   }
-
 }

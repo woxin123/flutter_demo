fa2153993c7185c31fdfceb97c4ed300b3f8017b
1293141942@qq.com <1293141942@qq.com>
2021-04-12 00:22:35 +0800

    avatar

--- flutter_personal/lib/home/body.dart
+++ flutter_personal/lib/home/body.dart
@@ -8,6 +8,9 @@ class PersonalAppBody extends StatelessWidget {
     return Padding(
       padding: EdgeInsets.only(top: 20.0),
       child: Container(
+        constraints: BoxConstraints(
+          minWidth: double.infinity,
+        ),
         width: MediaQuery.of(context).size.width,
         // child: Stack(
         //   alignment: Alignment.topCenter,
@@ -18,25 +21,25 @@ class PersonalAppBody extends StatelessWidget {
         //   ],
         child: Center(
           child: new Container(
-            width: MediaQuery.of(context).size.width * 0.8,
+            width: MediaQuery.of(context).size.width * 0.7,
             child: Row(
               crossAxisAlignment: CrossAxisAlignment.start,
               mainAxisSize: MainAxisSize.min,
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Expanded(
-                  child: _left(),
+                  child: _left(context),
                   flex: 1,
                 ),
                 SizedBox(
-                  width: 30.0,
+                  width: 10.0,
                 ),
                 Expanded(
                   child: Center(child: _center(context)),
-                  flex: 2,
+                  flex: 3,
                 ),
                 SizedBox(
-                  width: 30.0,
+                  width: 10.0,
                 ),
                 Expanded(
                   child: _right(),
@@ -50,12 +53,18 @@ class PersonalAppBody extends StatelessWidget {
     );
   }
 
-  Widget _left() {
+  Widget _left(BuildContext context) {
     return Container(
-      width: 100.0,
-      height: 500.0,
-      decoration: BoxDecoration(
-        color: Colors.green,
+      child: Column(
+        children: [
+          _personalMessage(),
+          Container(
+            height: 200.0,
+            decoration: BoxDecoration(
+                color: Colors.red,
+                borderRadius: BorderRadius.all(Radius.circular(10.0))),
+          )
+        ],
       ),
     );
   }
@@ -79,6 +88,44 @@ class PersonalAppBody extends StatelessWidget {
               color: Colors.red,
             ),
           ),
+        ],
+      ),
+    );
+  }
+
+  Widget _personalMessage() {
+    final double borderRadius = 5.0;
+    return Container(
+      decoration:
+          BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(borderRadius))),
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          Column(
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.only(topLeft: Radius.circular(borderRadius), topRight: Radius.circular(borderRadius)),
+                child: Image(
+                  height: 130.0,
+                  width: double.infinity,
+                  fit: BoxFit.none,
+                  alignment: Alignment.center,
+                  image: AssetImage("images/personal_message_area.png"),
+                ),
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              Text("一个很懒的程序员"),
+            ],
+          ),
+          Positioned(
+            top: 80.0,
+            child: CircleAvatar(
+              backgroundImage: AssetImage("images/touxiang.jpg"),
+              radius: 40.0,
+            ),
+          )
         ],
       ),
     );
